0f3001a18e15f16befd93189ee0a9d039a4d8778
Valiantsin <valiantsin.haradkou@icloud.com>
2021-04-18 18:46:49 +0300

    feat: data from API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://4mm9mzw46b.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://4mm9mzw46b.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://4mm9mzw46b.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://4mm9mzw46b.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://4mm9mzw46b.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
